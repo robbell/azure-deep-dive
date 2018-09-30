e49dca3f519af9b81a867ff5621ad1d8b264f219
Rob Bell <me@rob-bell.net>
2018-09-30 12:07:05 +0100

    Add simple ServiceBus pub/sub example

--- labs/service-bus/service-bus-publisher/Program.cs
+++ labs/service-bus/service-bus-publisher/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
 
-namespace service_bus_publisher
+namespace ServiceBus.Publisher
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string connectionString = "Endpoint=sb://robnamespace2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=usByg0LJsnkuZhRCCucFsy7EyOMv1XrSaLFlYUvQ5DM=";
+        private const string topicName = "MyTopic";
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new TopicClient(connectionString, topicName);
+
+            while (true)
+            {
+                Console.WriteLine("Enter your message:");
+
+                var message = Console.ReadLine();
+
+                client.SendAsync(new Message(Encoding.UTF8.GetBytes(message))).Wait();
+            }
         }
     }
 }
--- labs/service-bus/service-bus-subscriber/Program.cs
+++ labs/service-bus/service-bus-subscriber/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
-namespace service_bus_subscriber
+namespace ServiceBus.Subscriber
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static SubscriptionClient subscriptionClient;
+        private const string connectionString = "[Replace with your connectionString]";
+        private const string topicName = "MyTopic";
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the subscription name:");
+
+            var subscriptionName = Console.ReadLine();
+
+            subscriptionClient = new SubscriptionClient(connectionString, topicName, subscriptionName);
+
+            subscriptionClient.RegisterMessageHandler(
+                MessageReceivedHandler,
+                new MessageHandlerOptions(async handlerArgs => await Task.Run(() => Console.WriteLine(handlerArgs.Exception)))
+                {
+                    MaxConcurrentCalls = 1,
+                    AutoComplete = false
+                });
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static async Task MessageReceivedHandler(Message message, CancellationToken token)
+        {
+            var messageBody = Encoding.UTF8.GetString(message.Body);
+
+            Console.WriteLine(messageBody);
+
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
         }
     }
 }
