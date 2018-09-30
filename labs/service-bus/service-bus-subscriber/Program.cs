accc65b7cf472c24b7dc2e60a1e1af4894476096
Rob Bell <me@rob-bell.net>
2018-09-30 10:04:27 +0100

    Add Service Bus empty lab

---
+++ labs/service-bus/service-bus-subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace service_bus_subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
